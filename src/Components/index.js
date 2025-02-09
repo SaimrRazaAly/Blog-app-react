57317a37da2942d33484c1967eebd377629a584e
SaimRazaAly <saimrazateach2010@gmail.com>
2025-02-08 20:41:05 -0800

    Login and SignUp Components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,10 +1,21 @@
-import Header from "./Header/Header"
-import Footer from "./Footer/Footer"
-import Loading from "./Mini_Compos/Loading"
-import Container from "./Container/Container"
-import Logo from "./Mini_Compos/Logo"
-import LogoutBtn from "./Header/LogoutBtn"
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Loading from "./Mini_Compos/Loading";
+import Container from "./Container/Container";
+import Logo from "./Mini_Compos/Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import Select from "./Select";
 
-
-export { Header, Footer, Loading, Container, Logo, LogoutBtn };
-
+export {
+  Header,
+  Footer,
+  Loading,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  PostCard,
+  Select,
+};
